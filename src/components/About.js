934c7d72565762c793e9f32f12ea7a33957fdf69
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-10-02 17:02:23 -0500

    added content to the about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,15 @@ const About = () => {
 
 
   return (
-      <h1 style={{color: 'black'}}>This is the about page</h1>
+      <div>
+        <h1 style={{color: 'black'}}>Welcome to Pantry Pals!</h1>
+         <h3>What is Pantry Pal?</h3>
+         <p>First, a question for you. Have you ever lived with a lot of people and tried to keep a running list what what is needed for your house hold only to have everyone forget the list when they go to the store? </p>
+         <p>OR, you get home from the store only to realize, damn, we were out of ketchup! I should have gotten it after all! </p>
+         <p>Fear not! That is what Pantry Pal is here for.</p>
+         <p>Pantry Pal is designed to help you and your household figure out what you have in your pantry, (or fridge), at the moment and what you really need from the store. We want to make it easier for you at the store and at home. We know the living with people who have a varies of taste and preference can be difficult, and we want to make your household more harmonious in the kitchen. Because, we at Pantry Pals believe in good food and belly pantries with exactly what you need.</p>
+      </div>
+
   
   )
 
---
+++ src/components/Food.js
@@ -0,0 +1,8 @@
+import { EmojiFoodBeverage } from '@material-ui/icons';
+import React from 'react';
+
+const Food = () => {
+  
+}
+
+export default Food
