2bf06eac96632ebba3cacfe3508889975cfbe788
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-10-01 20:42:46 -0500

    updated home page view

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,52 @@ import ShoppingList from './ShoppingList';
 import Pantry from './Pantry';
 import PalList from './PalList';
 import UserProfile from './UserProfile'; 
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card'
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import cookie from 'cookie'
 
-//this is where you will display possible content 
-  //small boxes with 
-    //profile 
-    //shopping list 
-    //pantry 
-    //friends list 
-
-const Home = () => {
+const Home = (props) => {
 
   return (
-      <h1 style={{color: 'black'}}>This is the home page</h1>
+      <Grid
+        // container
+        component="main" 
+        direction="row"
+        justify="center"
+        alignItems="center"
+        // className={classes.root}
+        >
+        <h1>Welcome {props.bindUsername}</h1>
+        <Grid
+        item xs={12} sm={8} md={5} elevation={6} square>
+        <Card>
+          <Paper>
+            <ShoppingList/>
+          </Paper>
+        </Card>
+
+        <Card>
+          <Paper>
+            <Pantry/>
+          </Paper>
+        </Card>
+
+        <Card>
+          <Paper>
+            <PalList/>
+          </Paper>
+        </Card>
+
+        <Card>
+          <Paper>
+            <UserProfile/>
+          </Paper>
+        </Card>
+
+        </Grid>
+      </Grid>
   
   )
 
