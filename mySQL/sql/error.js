659b9f788aa474f8c9023dde33ee94e1d2ab6beb
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-10-09 20:59:47 -0500

    start to add backend.

--- mySQL/sql/error.js
+++ mySQL/sql/error.js
@@ -1,1 +1,5 @@
 //handle all your possible error codes 
+const handleSQLErrors = (res, err) => {
+  console.log('You have an SQL error', err)
+  return res.status(error).send('There was an unexpected error in your request')
+} 
--- mySQL/sqlIndex.js
+++ mySQL/sqlIndex.js
@@ -6,3 +6,8 @@ const port = process.env.PORT || 4000
 //need to connect up your sql and have the basic routes
 //remember to have the route protected
 //should be prefixed by your role, MOU
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+  console.log(`Under your backend, you are currently listening on port ${port}`)
+})
