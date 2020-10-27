f5e10a6658cb40eaa8d7e735ad8e0e8c1ff8962f
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-10-26 20:04:52 -0500

    latest version of the accept pal request

--- mySQL/controller/palList.js
+++ mySQL/controller/palList.js
@@ -19,9 +19,9 @@ pool.query(sql, (err, row) => {
 
 }
 
-const viewSentReq = () => {
+const viewSentReq = (req, res) => {
   //this is for request send by the current user. 
-  sql='SELECT pal FROM palListsRequests WHERE active=1 AND requesterUser=?'
+  sql='SELECT palRequestId AS reqId, pal AS waiting FROM palListsRequests WHERE active=1 AND requesterUser=?'
 
   sql=mysql.format(sql, [req.params.username])
 
@@ -31,9 +31,10 @@ const viewSentReq = () => {
   })
 }
 
-const viewPendingReq = () => {
+//this will be associated with the ability to accept! 
+const viewPendingReq = (req, res) => {
   //this are request sent by another user and can be approved.
-  sql='SELECT palRequestId, requesterUser FROM palListsRequests WHERE active=1 AND pal=?'
+  sql='SELECT palRequestId AS reqId, requesterUser AS replyOn FROM palListsRequests WHERE active=1 AND pal=?'
 
   sql.mysql.format(sql, [req.params.username])
 
@@ -47,13 +48,17 @@ const viewPendingReq = () => {
 //POST
 const sendPalReq = (req, res) => {
   console.log('send a request to become friends ')
+
+  const {askingUser } = req.body
 //write a query for approving or rejects a request 
 sql='INSERT INTO palListsRequests (requesterUser, pal) VALUES (?, ?)'
 
-sql=mysql.format(sql, (err, row) => {
-  if(err) handleSQLError(res, err)
-  return res.json(row)
-})
+sql=mysql.format(sql,[req.params.username, askingUser ])
+
+pool.query(sql, (err, results) => {
+  if(err) return handleSQLError(res, err)
+  return res.json( { newId: results.insertId} ); //double check this
+})  
 
 }
 
@@ -63,14 +68,42 @@ const blockPal = (req, res) => {
 }
 
 //PUT
+
+//this path should only be available for the viewPendingReq, so the pending mean the current using has the pal role on the table.  
 const acceptPalReq = (req, res) => {
   console.log('accept a pal request')
 //write a query that accepts a pal req
+
+//since the information should already be based as the key from the view requests and this action should only be turned on where the use is listed as the pal. 
+const { reqId } = req.body
+
+//this will be triggered by the pal role accepting, so all references to the pal will be the current user
+let sql= 'BEGIN; UPDATE palListsRequests SET approved=1, active=0 WHERE palRequestId= ? ; INSERT INTO access (username, pantry, pantryRole, shopList, shopListRole ,palReq) VALUES ((SELECT pal FROM palListsRequests WHERE palRequestId= ? ), (SELECT DISTINCT pS.pantrySettingId FROM pantriesSettings AS pS JOIN palListsRequests AS pLR ON pS.owner=pLR.requesterUser WHERE pLR.requesterUser=(SELECT requesterUser FROM palListsRequests WHERE palRequestId= ? ) ), 5, (SELECT DISTINCT sLS.shopListSetId FROM shopListsSettings AS sLS JOIN palListsRequests AS pLR ON sLS.owner=pLR.requesterUser WHERE pLR.requesterUser=(SELECT requesterUser FROM palListsRequests WHERE palRequestId= ? )),5 ,  ?);INSERT INTO access (username, pantry, pantryRole ,shopList, shopListRole, palReq) VALUES ( (SELECT requesterUser FROM palListsRequests WHERE palRequestId= ? ), (SELECT DISTINCT pS.pantrySettingId FROM pantriesSettings AS pS JOIN palListsRequests AS pLR ON pS.owner=pLR.pal WHERE pLR.pal=(SELECT pal FROM palListsRequests WHERE palRequestId= ?) ), 5, (SELECT DISTINCT sLS.shopListSetId FROM shopListsSettings AS sLS JOIN palListsRequests AS pLR ON sLS.owner=pLR.pal WHERE pLR.pal=(SELECT pal FROM palListsRequests WHERE palRequestId= ? )), 5,  ?); COMMIT'
+
+sql=mysql.format(sql,[reqId])
+
+pool.query(sql, (err, results) => {
+  if (err) return handleSQLError(res, err)
+  return res.status(204).json();
+})
+
 }
 
+//since the information should already be based as the key from the view requests and this action should only be turned on where the use is listed as the pal. 
 const declinePalReq = (req, res) => {
   console.log('decline a pal request')
 //write a query that decline a pal request
+const {reqId} = req.body
+
+let sql='UPDATE palListsRequests SET approved=0, active=0 WHERE palRequestId=?'
+
+sql= mysql.format(sql, [reqId])
+
+pool.query(sql, (err, results) => {
+  if(err) return handleSQLError(res, err)
+  return res.status(204).json(); 
+})
+
 }
 
 const cancelPalReq = (req, res) => {
--- mySQL/controller/shopList.js
+++ mySQL/controller/shopList.js
@@ -6,7 +6,9 @@ const pool = require('../sql/connection')
 const myShopList = (req, res) => {
   console.log('get the shoppingList for this user')
 //write a query that returns the shopping List for the user currently logged in  
-let sql = ''
+
+//this is a work in process, it is not done ~ we need to account for merges 
+let sql = 'SELECT sLS.shopListSetId AS listId,sLS.shopListName AS listName FROM shopListsSettings AS sLS WHERE owner=?'
 
 sql=mysql.format(sql,[req.params.username])
 
@@ -17,19 +19,48 @@ pool.query(sql, (err, row) => {
 
 }
 
+const viewShopRequests = (req, res) => {
+  console.log('owner and co owners should be abel to view these items')
+}
+
 //POST 
 const addToShopList = (req, res) => {
   console.log('add item to the shopping list')
 //write a query that add items to the list
+  const { listId, quantity, measure, item, spoon} = req.body
+
+  let sql = 'INSERT INTO shoppingLists (shopList, quantity, measId, item, spoonId) VALUES (?, ?, ?, ?, ?);'
+  
+  sql=mysql.format(sql, [ listId, quantity, measure, item, spoon ])
+
+  pool.query(sql, (err, row) => {
+    if(err) return handleSQLError(res, err)
+    return res.json( { newId: results.insertId} );
+  })
+
 }
 
 const addShopRequest = (req, res) => {
   console.log('add item to be requested')
+
+  const {} = req.body
+
+  //work in progress - needs to be some kind of join to verify the access as a requesters 
+  sql='INSERT INTO itemRequest (requester, quantity, measId, item, spoonId, shopList, access)'
+
+  sql=mysql.format(sql, [])
+
+  pool.query(sql, (err, row) => {
+    if(err) return handleSQLError(res, err)
+    return res.json( { newId: results.insertId} );
+  })
+
 }
 
 //PUT 
 const approveShopRequest = (req, res) => {
   console.log('approved the request') 
+
 }
 
 const declineShopRequest = (req, res) => {
@@ -58,6 +89,7 @@ const updateAutoAddPantry = (req, res) => {
 
 module.exports = { 
   myShopList,
+  viewShopRequests,
   addToShopList,
   addShopRequest,
   approveShopRequest,
