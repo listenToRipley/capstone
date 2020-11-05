aaffc85e446a7bc048f666b2b08081b67b7eee12
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-11-04 18:47:36 -0600

    change the query for pal list a little more

--- mySQL/controller/appFunc/palLists/usersPalList.js
+++ mySQL/controller/appFunc/palLists/usersPalList.js
@@ -8,7 +8,7 @@ const {handleSQLError} = require('../../../sql/error')
 const myPalList = (req, res) => {
   console.log('get the pal list of the current user ')
 //NEED TO UPDATE TO INCLUDE THE PAL LIST - we need to change this so the roles display and not the index
-let sql = 'pLS.palListName, pL.palList, pL.entryId, pL.username, pL.reqId FROM palLists AS pL JOIN palListsSettings AS pLS ON pL.palList=pLS.palListSettingsId WHERE pLS.owner=?'
+let sql = 'SELECT DISTINCT pL.username AS pals, pL.reqId AS palReq, pL.palList, pLS.palListName, a.pantryRole, a.shopListRole FROM palLists AS pL JOIN palListsRequests AS pLR JOIN palListsSettings AS pLS JOIN access AS a ON pLR.palRequestId=pL.reqId AND a.palReq=pL.reqId AND pL.palList=pLS.palListSettingsId WHERE pLR.approved=1 AND a.active=1 AND pLS.owner=?'
 
 sql=mysql.format(sql, [req.params.user])
 
