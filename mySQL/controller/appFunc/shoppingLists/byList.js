3498316e23dba18ec7ca4361d6d81c21953f678d
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-11-04 22:27:16 -0600

    last query updated the wrong query, fixed

--- mySQL/controller/appFunc/shoppingLists/byList.js
+++ mySQL/controller/appFunc/shoppingLists/byList.js
@@ -5,7 +5,7 @@ const { handleSQLError } = require('../../../sql/error')
 //GET
 const shopListDetails = (req, res) => {
   //this should be the detailed information about a specific request
-  let sql='SELECT * FROM shopListsSettings WHERE shopListSetId= ?'
+  let sql='SELECT a.shopList, sLS.owner, sLS.shopListName, a.username AS pals, a.shopListRole, a.palReq, sLS.autoAdd, sLS.mergeStatus FROM shopListsSettings AS sLS JOIN access AS a ON a.shopList=sLS.shopListSetId WHERE a.active=1 AND sLS.active=1 AND a.username<>sLS.owner AND a.shopList= ? ORDER BY a.username'
 
   sql=mysql.format(sql, [req.params.listId])
 
@@ -19,7 +19,7 @@ const shopListDetails = (req, res) => {
 
 const shopListCount = (req, res) => {
 //provide a count of how many items are currently on your shopping list 
-  let sql= 'SELECT a.shopList, sLS.owner, sLS.shopListName, a.username AS pals, a.shopListRole, a.palReq, sLS.autoAdd, sLS.mergeStatus FROM shopListsSettings AS sLS JOIN access AS a ON a.shopList=sLS.shopListSetId WHERE a.active=1 AND sLS.active=1 AND a.username<>sLS.owner AND a.pantry= ? ORDER BY a.username;'
+  let sql= 'SELECT COUNT(item) FROM shoppingLists WHERE shopList= ?'
 
   sql=mysql.format(sql, [req.params.listId])
 
@@ -36,7 +36,7 @@ const thisShopList = (req, res) => {
 //write a query that returns the shopping List for the user currently logged in  
 
 //this is a work in process, it is not done ~ we need to account for merges 
-let sql = 'SELECT * FROM shoppingLists WHERE activeItem=1 AND shopList= ? '
+let sql = 'SELECT * FROM shoppingLists WHERE activeItem=1 AND shopList= ? ORDER BY item '
 
 sql=mysql.format(sql,[req.params.listId])
 
@@ -54,7 +54,7 @@ const addToShopList = (req, res) => {
   const { quantity, measure, item, spoon} = req.body
   const {listId} = req.params
 
-  let sql = 'INSERT INTO shoppingLists (shopList, quantity, measId, item, spoonId) VALUES (?, ?, ?, ?, ?) ORDER BY item'
+  let sql = 'INSERT INTO shoppingLists (shopList, quantity, measId, item, spoonId) VALUES (?, ?, ?, ?, ?) '
   
   sql=mysql.format(sql, [ listId, quantity, measure, item, spoon ])
 
