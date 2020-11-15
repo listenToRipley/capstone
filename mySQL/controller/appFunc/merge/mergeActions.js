f21eefb512ff50a5a44cbf15c6065b6b39c34b5a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-11-14 22:26:52 -0600

    updated mergeaction to reflect token changes

--- mySQL/controller/appFunc/merge/mergeActions.js
+++ mySQL/controller/appFunc/merge/mergeActions.js
@@ -8,11 +8,10 @@ const sendMergeReq = (req, res) => {
   //remember the requesters will the one who whose pantry will now be the primary owner and the person who requests will be come the co-owner. 
   //the person who send this will have the role of requesters, to req.params.useris already there
   const {pal} = req.body
-  const {user} = req.params
 
   let sql='INSERT INTO mergeRequests (requester, mergePal, palReq) VALUES (?, ? , (SELECT palRequestId FROM palListsRequests WHERE requesterUser=?  AND pal= ? OR requesterUser= ? AND pal=? ))'
 
-  sql=mysql.format(sql,[ user, pal, pal, user, user, pal ])
+  sql=mysql.format(sql,[ req.user, pal, pal, req.user, req.user, pal ])
   
   pool.query(sql, (err, results) => {
     if(err) return handleSQLError(res, err)
@@ -22,22 +21,24 @@ const sendMergeReq = (req, res) => {
 }
 
 //PUT
+
+//make this its own document 
 const acceptMergeReq = (req, res) => {
   console.log('accept merge request')
 //remember the requesters will the one who whose pantry will now be the primary owner and the person who requests will be come the co-owner. 
   //must update the role if the merge request is approved. will become co-owner 
-  const {user, mergeId} = req.params
+  const {mergeId} = req.params
 //write a query for approving or rejects a request 
 let sql='BEGIN;UPDATE mergeRequests SET active=0, approved=1 WHERE mergeReqId= ?;UPDATE access SET pantryRole=3, shopListRole=3 WHERE username= ? AND palReq=(SELECT palRequestId FROM palListsRequests WHERE requesterUser= ? AND pal=(SELECT requester FROM mergeRequests WHERE mergeReqId= ? ) OR requesterUser=(SELECT requester FROM mergeRequests WHERE mergeReqId= ? ) AND pal= ? ); UPDATE access SET active=0 WHERE pantryRole=2 AND shopListRole=2 AND username= ? ; UPDATE pantries SET pantry=(SELECT pantrySettingId FROM pantriesSettings WHERE owner=(SELECT requester FROM mergeRequests WHERE mergeReqId= ? )) WHERE pantry=(SELECT pantrySettingId FROM pantriesSettings WHERE owner= ? ); UPDATE shoppingLists SET shopList=(SELECT shopListSetId FROM shopListsSettings WHERE owner=(SELECT requester FROM mergeRequests WHERE mergeReqId= ? )) WHERE shopList=(SELECT shopListSetId FROM shopListsSettings WHERE owner= ? ); UPDATE pantriesSettings SET mergeStatus= ? , active=0 WHERE owner= ? ; UPDATE shopListsSettings SET mergeStatus= ? , active=0 WHERE owner= ? ; UPDATE pantriesSettings SET mergeStatus= ? WHERE owner=(SELECT requester FROM mergeRequests WHERE mergeReqId= ? ); UPDATE shopListsSettings SET mergeStatus= ? WHERE owner=(SELECT requester FROM mergeRequests WHERE mergeReqId= ? ); COMMIT;'
 
 sql=mysql.format(sql,[ mergeId, 
-  user, user, 
+  req.user, req.user, 
   mergeId, mergeId, 
-  user, user, 
-  mergeId, user, 
-  mergeId, user, 
-  mergeId, user, 
-  mergeId, user,
+  req.user, req.user, 
+  mergeId, req.user, 
+  mergeId, req.user, 
+  mergeId, req.user, 
+  mergeId, req.user,
   mergeId, mergeId, mergeId, mergeId ])
 
 pool.query(sql, (err, results) => {
