78d739d48b0e02561da34f3208fd57537a702c9e
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-11-17 11:17:24 -0600

    removed unnesscary content from html

--- src/components/About.js
+++ src/components/About.js
@@ -34,7 +34,7 @@ const About = () => {
           <p>First, a question for you. Have you ever lived with a lot of people and tried to keep a running list what what is needed for your house hold only to have everyone forget the list when they go to the store? </p>
          <p>OR, you get home from the store only to realize, damn, we were out of ketchup! I should have gotten it after all! </p>
          <p>Fear not! That is what Pantry Pal is here for.</p>
-         <p>Pantry Pal is designed to help you and your household figure out what you have in your pantry, (or fridge), at the moment and what you really need from the store. We want to make it easier for you at the store and at home. We know the living with people who have a varies of taste and preference can be difficult, and we want to make your household more harmonious in the kitchen. Because, we at Pantry Pals believe in good food and belly pantries with exactly what you need.</p>
+         <p>Pantry Pal is designed to help you and your household figure out what you have in your pantry, (or fridge), at the moment and what you really need from the store. We want to make it easier for you at the store and at home. We know the living with people who have a varies of taste and preference can be difficult, and we want to make your household more harmonious in the kitchen. Because, we at Pantry Pals believe in good food, full bellies and even fuller pantries with exactly what you need to whip up your favorite meals.</p>
           </div>
       </Paper>
 
