8abda527e1962721e2cafa44827d3e7f368b40be
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-11-17 11:28:04 -0600

    added notes to food

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,6 +1,7 @@
 import { EmojiFoodBeverage } from '@material-ui/icons';
 import React from 'react';
 
+//this should not be based on current user, but add will always add to the primary list of the current user
 //this will be based on spoon, need to be able to search and add to shopping list or pantry 
 const Food = () => {
   <div>
