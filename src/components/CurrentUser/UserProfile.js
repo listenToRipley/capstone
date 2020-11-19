9df300f4ed02fcb3287b6cb992492b67b092675c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-11-18 21:41:07 -0600

    outlined the user profile

--- src/components/CurrentUser/UserProfile.js
+++ src/components/CurrentUser/UserProfile.js
@@ -21,7 +21,48 @@ import React from 'react';
 const UserProfile = () => {
 
   return (
-    <div>User Profile</div>
+    <div>
+      <button>Edit</button>
+      <h1>USERNAME Profile</h1>
+      <div>
+      <h2>User info</h2>
+      <p>Name</p>
+      <p>password - edit mode only</p>
+      <p>birthday</p>
+      <h4>location</h4>
+        <div>
+          <p>address</p>
+          <p>city</p>
+          <p>state</p>
+          <p>country</p>
+        </div>
+        <h4>contact info</h4> 
+          <div>
+            <p>email</p>
+            <p>phone</p>
+          </div>
+      </div>
+      <div>
+      <h3>Preferences</h3>
+        <p>likes</p>
+        <p>dislikes</p>
+        <p>allergies</p>
+        <p>diets</p>
+      </div>
+
+      <div>
+        <h3>Display Preferences - update by toggle</h3>
+        <p>birthday</p>
+        <p>location</p>
+        <p>email</p>
+        <p>phone</p>
+        <p>likes</p>
+        <p>dislikes</p>
+        <p>allergies</p>
+        <p>diets</p>
+      </div>
+      <button>Save</button>
+    </div>
   )
 }
 
