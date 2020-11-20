f8e27abc0a96f555a476cbff779b9de70dfcb6f7
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-11-19 20:47:42 -0600

    added notes to pal list actions

--- src/components/PalLists/PalListActions.js
+++ src/components/PalLists/PalListActions.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const PalListActions = () => {
   const handleNameUpdate = () => {
-
+    //add action here 
   }
+
+  return (
+    <div>
+      This will allow the name to be updated 
+    </div>
+  )
 }
 
 export default PalListActions
