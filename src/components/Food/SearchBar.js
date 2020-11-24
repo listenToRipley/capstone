e3dd59be424357809ae365d39b9de9fa74412ac1
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-11-24 16:52:44 -0600

    update searchbar for food

---
+++ src/components/Food/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import MenuItem from '@material-ui/core/MenuItem'
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+const foodSearchBar = () => {
+
+  const handleSearch = () => {
+
+  }
+
+  <div className='searchbar'>
+  <MenuItem className='search'>
+   <div className='searchIcon'>
+     <SearchIcon />
+   </div>
+   <InputBase
+     placeholder="Search…"
+     className={'inputRoot','inputInput'}
+     
+     inputProps={{ 'aria-label': 'search' }}
+   />
+  </MenuItem>
+ </div>
+
+}
+
