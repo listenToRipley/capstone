371c3aae5fbf3f8e8ed821ca218c6ec9cc3a3657
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-11-23 20:05:25 -0600

    removed unneeded divs from user profile

--- src/components/CurrentUser/UserProfile/Main.js
+++ src/components/CurrentUser/UserProfile/Main.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import {useState} from 'react'
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import InputLabel from '@material-ui/core/InputLabel';
-import Box from '@material-ui/core/Box'
-import { shadows } from '@material-ui/system';
 import Paper from '@material-ui/core/Paper';
-import Container from '@material-ui/core/Container';
-import Chip from '@material-ui/core/Chip';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { withStyles } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Switch from '@material-ui/core/Switch';
-import WifiIcon from '@material-ui/icons/Wifi';
-import BluetoothIcon from '@material-ui/icons/Bluetooth';
 // import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import {useInput} from '../../../Hooks/inputHook';
@@ -72,60 +55,13 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     [theme.breakpoints.up('sm')]: {
       display: 'block',
-    },
-  },
-  subtitle: {
-    padding: 20,
-  },
-  fields: {
-    margin: theme.spacing(4),
-    width: '90%',
-
-    },
-    fieldLabel: {
-      padding: theme.spacing(1),
-    },
-  form: {
-    width: '100vw', // Fix IE 11 issue.
-    paddingLeft: '5%',
-    margin: theme.spacing(1),
-    // marginBottom: theme.spacing(1),
-  },
-  tastes: {
-    width: '80%', // Fix IE 11 issue.
-    paddingLeft: '5%',
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-    borderRadius: 3
-  },
-  displayPrefCell: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-    },
+    }
+  }
 }));
 
-const UserProfile = () => {
+const UserProfile = (props) => {
   const classes = useStyles();
   //need to populate originally from the sign in info of the user
-  const [checked, setChecked] = React.useState(['wifi']);
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
-  const handleUpdates = () => {
-    //would be better to have an on handle change? 
-  }
 
   return (
     <div className={classes.root}>
@@ -136,24 +72,11 @@ const UserProfile = () => {
       variant="h4"
       >  USERNAME Profile </Typography>
       <Card>
-
-        <div>
-         <Info/>
-        <div>
-          <Location/>
-        </div>
-
-        <div>
-          <Contact/>
-        </div>
-        </div>
-        <div>
-        <Tastes/>
-      </div>
-
-        <div>
-          <DisplayPreferences/>
-        </div>
+        <Info user={props.username}/>
+        <Location user={props.username}/>
+        <Contact user={props.username}/>
+        <Tastes user={props.username}/>
+        <DisplayPreferences user={props.username}/>
         <button>Save</button>
       </Card>
     </Paper>
