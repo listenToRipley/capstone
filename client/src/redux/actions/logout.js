66baa65f76d9669a1e030d8544c5bf2947006b11
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-02 21:56:17 -0600

    need to work on the logout

--- client/src/redux/actions/logout.js
+++ client/src/redux/actions/logout.js
@@ -1,8 +1,9 @@
 import {LOGOUT} from './types'
 
 export const logout = (boo) => {
+  console.log('hey, you want to logout?')
   return {
     type: LOGOUT,
-    payload: ''
+    payload: boo
   }
 }
