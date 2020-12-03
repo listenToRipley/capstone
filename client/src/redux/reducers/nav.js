d87b3f300402b4c0d46bf67cec7511fdfbdaf7da
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-03 17:41:22 -0600

    log out is now working

--- client/src/redux/reducers/nav.js
+++ client/src/redux/reducers/nav.js
@@ -3,16 +3,17 @@ import state from '../state'
 
 let startingState = {...state}
 
-console.log('this is the state for the nav bar', startingState)
+console.log('this is the state for the nav bar - user', startingState.user)
 
 export default function(state ={...startingState}, action) {
   console.log('action payload on nav bar', action.payload)
   switch(action.type) {
     case USER_INFORMATION: 
-    let newState = [{...state}]
+    let newState = {...state}
     return newState = action.payload
     case LOGOUT:
-    return state
+    let reset = {...state}
+    return reset
     default: return state
   }
 }
