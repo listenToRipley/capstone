423dc97a7b4dfc3b7b5f35d9fe97af8a14344f1d
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-06 11:36:35 -0600

    action was purel

--- client/src/redux/reducers/userShopList.js
+++ client/src/redux/reducers/userShopList.js
@@ -5,9 +5,9 @@ let initialState = {...state}
 //this isn't getting hit right now. 
 console.log('state is shop list? ', state)
 
-export default function(state = {...initialState}, actions){
-  console.log('make sure I understand the ', state, 'and ', actions)
-  switch(actions.type) {
+export default function(state = {...initialState}, action){
+  console.log('make sure I understand the ', state, 'and ', action)
+  switch(action.type) {
     case ITEMS_ON_SHOPPING_LIST: 
     return [action.payload.shopList]
     default: return state
