995bb31fa4821ba9bfb5fff6ff2585901547fa45
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-05 18:40:03 -0600

    updated input for post in action

--- client/src/redux/actions/createNewUser.js
+++ client/src/redux/actions/createNewUser.js
@@ -1,13 +1,21 @@
 import {CREATE_NEW_USER} from './types'
 import moment from 'moment'
 
-export const createNewUser = (pass, user) => async dispatch => {
-
+export const createNewUser = (username, firstName, lastName, email, password, dobMonth, dobDay, dobYear) => async dispatch => {
+  console.log('the input looks like? ', username, firstName, lastName, email, password, dobMonth, dobDay, dobYear)
   try{
       let res = await fetch('/preLogin/createUser', {
         method: 'POST',
         headers: {
           Accept: "application/json", "Content-Type": "application/json",
+          "username": `${username}`,
+          "password": `${password}`,
+          "email": `${email}`,
+          "firstName": `${firstName}`,
+          "lastName": `${lastName}`,
+          "dobMonth": dobMonth,
+          "dobDate": dobDate,
+          "dobYear": dobYear
         }
       }
       )
