889c2deb3e00ba1cc46002a09e2730d842db5580
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-05 19:24:23 -0600

    create page for completed new create

---
+++ client/src/Components/Congrads.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
+const Congrads = () => {
+  //should add something here to reset the state of the newUser state
+  return (
+    <Paper>
+      <Typography>
+      Congratulations! You are now a Pantry Pal user! 
+      </Typography>
+       <Link to={{
+         pathname:'/'
+       }}>Log in Here</Link>
+    </Paper>
+  )
+}
+
+export default Congrads
