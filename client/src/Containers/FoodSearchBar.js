9b00774f3fa53b84173a3a8038cff141874ef634
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-07 19:26:11 -0600

    outlined the container for food search

--- client/src/Containers/FoodSearchBar.js
+++ client/src/Containers/FoodSearchBar.js
@@ -1,1 +1,15 @@
 //search bar, results should create the food list 
+import {connect} from 'react-redux'
+import FoodSearchBar from '../Components/FoodSearchBar'
+
+const mapStateToProps = state => {
+  return {
+    foodSearchList: state.foodSearchList
+  }
+}
+
+const mapStateToDispatch = (dispatch) => {
+  return {
+    
+  }
+}
