6f052fe0594a30c5b0efc5a1a8dce6a58a3ee348
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-07 19:54:18 -0600

    more comments

--- client/src/redux/reducers/createNewUser.js
+++ client/src/redux/reducers/createNewUser.js
@@ -2,7 +2,6 @@ import {CREATE_NEW_USER} from '../actions/types'
 import state from '../state'
 
 let initialState = {...state}
-console.log('state is what for new user? ', state)
 
 export default function(state = {...initialState}, actions){
   switch(actions.type) {
