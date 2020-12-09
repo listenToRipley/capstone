869229543bc0d90b2c7e8c3e73f50555a6355b7a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-09 11:19:38 -0600

    added actions to shoplist reducer

--- client/src/redux/reducers/userShopList.js
+++ client/src/redux/reducers/userShopList.js
@@ -1,4 +1,4 @@
-import {YOUR_SHOPPING_LIST} from '../actions/types'
+import {YOUR_SHOPPING_LIST, ADD_ITEM_TO_SHOPPING_LIST} from '../actions/types'
 import state from '../state'
 
 let initialState = {...state}
@@ -8,6 +8,8 @@ export default function(state = {...initialState.userShopList}, action) {
   switch(action.type) {
     case YOUR_SHOPPING_LIST:
     return action.payload.userShopList
+    case ADD_ITEM_TO_SHOPPING_LIST: 
+    return action.payload.userShopList //the result of this action should be an amended shop list 
     default: return state
   }
 }
