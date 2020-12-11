42bee8cbc2f869fc918baf82e7e29e7767a0bb11
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-11 00:05:28 -0600

    updated routes

--- client/src/Router/Router.js
+++ client/src/Router/Router.js
@@ -6,22 +6,10 @@ import ShoppingList from '../Containers/ShoppingList';
 import CreateUser from '../Containers/CreateUser';
 import About from '../Components/About'
 import Pantry from '../Containers/Pantry';
-
 import cookie from 'cookie'
 
-//futureStateItems
-// import Inbox from '../Components/CurrentUser/Inbox/Inbox'
-// import ForgotPassword from '../Components/AppFunc/ForgotPassword'
-
-// import PalList from '../Components/PalLists/PalsList';
-// import UserProfile from '../Components/CurrentUser/UserProfile/Main';
-// import PalProfile from '../Components/PalProfile/PalProfile'
-
-
-// console.log(cookie)
-//this will be for when you move to login required status
 const checkAuth = () => {
-  // associate with token created during login? 
+ 
   const cookies = cookie.parse(document.cookie)
   const status = JSON.parse(cookies.logCookies).validation
   if (status) {
@@ -40,8 +28,6 @@ const ProtectedRoute = ({component: Component, ...rest}) => {
   )
 }
 
-
-//would like everything but the login to be protected routes, the route can be protected through the user name, if I can pass it correctly 
 const Router = () => {
 
   return(
@@ -50,7 +36,7 @@ const Router = () => {
       <Route path="/home" component={Home}/>
       <Route path="/shoppingList" component={ShoppingList}/>
       <Route path="/pantry" component={Pantry}/>
-{/* un protected routes */}
+
       <Route exact path="/" component={LogIn}/>
       <Route path="/createNewUser" component={CreateUser}/>
       <Route path="/about" component={About}/>
@@ -60,10 +46,3 @@ const Router = () => {
 }
 
 export default Router
-
-//futureStateRoutes
-// <Route path="/forgotpassword" component={ForgotPassword}/>
-// <Route path="/palProfile" component={PalProfile}/>
-// <Route path="/inbox" component={Inbox}/>
-// <Route path="/palsList" component={PalList}/>
-// <Route path="/userProfile" component={UserProfile}/>
