bf249fa87e720bf71208efb80fd25b2f5dc3df6a
Jon Gucciardi <jon.gucciardi@catapultsystems.com>
2020-12-12 15:06:34 -0600

    resolve

--- sqlIndex.js
+++ sqlIndex.js
@@ -26,7 +26,7 @@ app.use('/preLogin',preLogin)
 app.use('/postLogin', postLogin)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"))
+  res.sendFile(path.resolve(__dirname, "/client/build", "index.html"))
 })
 
 app.listen(port, () => {
