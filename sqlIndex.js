087ef3dc484b20f21ff20b32ebed97e5351291f4
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-12-12 12:05:50 -0600

    verify changes added

--- sqlIndex.js
+++ sqlIndex.js
@@ -10,16 +10,6 @@ const log = require('./routers/login/logIn')
 const preLogin = require('./routers/pre/admin')
 const postLogin = require('./routers/login/currentUser')
 
-app.use(express.static(path.join(__dirname, './client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
-})
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Pantry Pal Server!')
-})
-
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(express.json())
@@ -29,7 +19,11 @@ app.use('/login', log)
 app.use('/preLogin',preLogin)
 app.use('/postLogin', postLogin)
 
+app.use(express.static(path.join(__dirname, './client/build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Under your backend, you are currently listening on port ${port}`)
