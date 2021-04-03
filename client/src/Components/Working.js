a5760e9cc3e3153b5a165901bc205a1f933652e3
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-03 15:55:11 -0500

    outline css on working

--- client/src/Components/Working.js
+++ client/src/Components/Working.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import working from './Img/work.gif'
 
+const useStyles = makeStyles((theme) => ({ 
+
+  })
+);
+
 const Working = () => {
+
+  const classes = useStyles();
+
   return (
     <div>
       <img src={working}/>
