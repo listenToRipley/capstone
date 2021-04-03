95a65cc2f8a102593c14cb121bf35caa017311b0
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-04-03 16:02:10 -0500

    csss on working top looks good

--- client/src/Components/Working.js
+++ client/src/Components/Working.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import working from './Img/work.gif'
 
 const useStyles = makeStyles((theme) => ({ 
-
+  root: {
+    display: 'flex',
+    position: 'absolute !important' 
+  },
+  toolbar: theme.mixins.toolbar,
+  appBar: {
+    toolbar: theme.mixins.tool,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
   })
 );
 
@@ -16,6 +29,17 @@ const Working = () => {
 
   return (
     <div>
+      <div className={classes.root} style={{position: "fixed"}}></div>
+      <AppBar
+        position="fixed"
+        className={classes.appBar}
+      >
+        <Toolbar position="fixed">
+          <Typography className={classes.title} variant="h6" noWrap>
+            Pantry Pals
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <img src={working}/>
     </div>
   )
